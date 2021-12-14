2355a4541b2c14fbdb95389cb414e888bc935844
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2021-12-14 16:17:29 +0200

    Fix font size in quiz

--- src/main/java/fi/digiprojekti/Oppio/web/OppioController.java
+++ src/main/java/fi/digiprojekti/Oppio/web/OppioController.java
@@ -1,7 +1,6 @@
 package fi.digiprojekti.Oppio.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
